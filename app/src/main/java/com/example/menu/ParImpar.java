feb28af5a39312b6861226422c9a701c7802d8fe
b3nj4hb <benjalive12@gmail.com>
2022-09-12 10:54:35 -0500

    activity

---
+++ app/src/main/java/com/example/menu/ParImpar.java
@@ -0,0 +1,14 @@
+package com.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParImpar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_par_impar);
+    }
+}
